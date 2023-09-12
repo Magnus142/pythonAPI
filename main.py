983c3ede6f35775303725f41e15af12915163aa8
Magnus <magrek20@o365.mrvgs.no>
2023-09-12 13:36:40 +0200

    la til ting og gjorde ting bedre, kan nå toggle valg og bruke de valgene til å søke

--- main.py
+++ main.py
@@ -6,6 +6,27 @@ fortsett = True
 link = False
 snippet = False
 
+
+def funkerdetint(string,min,max):   # gjør slik at man kun kan skrive inn kun en integer mellom to verdier
+    tallk = False
+    while tallk == False:
+        try:
+            print("\n"+string)
+            testet = int(input("skriv her: "))
+            if testet > max or testet < min:    # om integeren er større eller mindre enn det jeg ønsker blir det en error
+                error
+            tallk = True
+        except:
+            print("\n\n████ Skriv inn et heltall mellom",min,"og",max,"████")
+    return testet
+
+
+
+
+
+
+
+
 def google_search(search_term, api_key, cse_id, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
     res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
@@ -15,33 +36,38 @@ def google_search(search_term, api_key, cse_id, **kwargs):
 
     # Main
 search = input("Hva ønsker du å søke på?: ")
+search_result = google_search(search, my_api_key, my_cse_id)
 while fortsett:
-    search_result = google_search(search, my_api_key, my_cse_id)
     print("\n\n\n\nDu søker etter:",search+", hva ønsker du å få opp?")
 
-    if link == False:
-        print("1: Link til nettside")
-    elif:
-        print("1Link til nettside")
 
-    print("2: En snippet fra nettside")
+    if link == False:
+        print("1: Link til nettside     x")
+    elif link == True:
+        print("1: Link til nettside     ✓")
+    if snippet == False:
+        print("2: En snippet fra nettside   x")
+    elif snippet == True:
+        print("2: En snippet fra nettside   ✓")
     print("\n3: Søk med disse valgene")
 
 
-    valg = input("\n Skriv her: ")
+    valg = int(funkerdetint("", 1, 3))
+
     if valg == 1:
-        link == True
+        if link == False:
+            link = True
+        else:
+            link = False
     elif valg == 2:
-        snippet = True
-
-
-
-
-
-
-
-
-
-print("\nFørste snippet for",search+":")
-print(search_result["items"][0]["snippet"])
-print(search_result["items"][0]["link"])
+        if snippet == False:
+            snippet = True
+        else:
+            snippet = False
+    else:
+        fortsett = False
+        print("\n\n\n")
+        if link == True:
+            print("Link: ",search_result["items"][0]["link"])
+        if snippet == True:
+            print("Snippet: ",search_result["items"][0]["snippet"])
